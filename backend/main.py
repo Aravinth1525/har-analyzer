8bc346a39df24151bae00aadab0044e1d359e515
Aravinth1525 <aravinth272@gmail.com>
2026-02-27 01:01:47 +0530

    Re-Routing

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import json
 from analyzer import analyze_har
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def home():
+    return FileResponse("static/index.html")
 
 app = FastAPI()
 
